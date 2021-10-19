0a0ba663a34281dc2036ce8475087f467cafdc07
perla velazquez <perlavelazquez@MacBook-Pro-2.local>
2021-10-19 09:54:04 -0700

    fix JS

--- script.js
+++ script.js
@@ -23,42 +23,50 @@ function formatDate(timestamp) {
 
   return `${day} ${hours}:${minutes}`;
 }
-function formatDay(timestamp){
+function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-   return days[day];
+  return days[day];
 }
-}
 
-
-function displayForecast(response){
+function displayForecast(response) {
   let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function(forecastDay, index){
-    if (index < 5){
-    forecastHTML = forecastHTML + `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-              <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+              <div class="weather-forecast-date">${formatDay(
+                forecastDay.dt
+              )}</div>
               <img
-                src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
                 width="42"
               />
               <div class="weather-forecast-temperature">
-                <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}º</span>
-                <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}º</span>
+                <span class="weather-forecast-temperature-max">${Math.round(
+                  forecastDay.temp.max
+                )}º</span>
+                <span class="weather-forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}º</span>
               </div>
             </div>
             `;
     }
-  })
+  });
   forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML
+  forecastElement.innerHTML = forecastHTML;
   console.log(forecastHTML);
 }
 function getForecast(coordinates) {
@@ -80,10 +88,10 @@ function displayWeatherForcast(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-    
-    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
-    getForecast(response.data.coord);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  getForecast(response.data.coord);
 }
 let celsiusTemperature = null;
 
